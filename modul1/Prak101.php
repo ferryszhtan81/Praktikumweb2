36dd19030db421a0a0f3d7ae828f6adbe7481f75
Ferry Roland <101640525+ferryszhtan81@users.noreply.github.com>
2022-03-17 16:23:06 +0800

    Update Prak101.php

--- modul1/Prak101.php
+++ modul1/Prak101.php
@@ -4,8 +4,10 @@
     <title>PRAK101.php</title>
 </head>
 <body>
-<p><?php echo "Hello World in PHP" ?></p>
-<p><?php  echo "Nama Praktikan : Ferry Roland"?></p>
-<p><?php echo "NIM:  2010817210016" ?></p>
+<?php
+echo "Hello World in PHP";
+echo "Nama Praktikan: Ferry Roland";
+echo "NIM: 2010817210016";
+?>
 </body>
 </html>
