54e734fa2791b80a86e0a2901931a6517cfeeac6
Ferry Roland <101640525+ferryszhtan81@users.noreply.github.com>
2022-03-22 22:16:01 +0800

    Create Prak103.php

---
+++ modul1/Prak103.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>PRAK103.php</title>
+</head>
+<body>
+<?php
+$Celsius = 37.841;
+$Fahrenheit = 9/5*$Celsius+32;
+$Reaumur = 4/5*$Celsius;
+$Kelvin = $Celsius+273;
+
+echo "Celsius = ".$Celsius."<br />";
+echo "<br />Fahrenheit = ".$Fahrenheit;
+echo "<br />Reamur = ".$Reaumur;
+echo "<br />Kelvin = ".$Kelvin;
+?>
+</body>
+</html>
