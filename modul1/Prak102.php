5abb62dbae36c6baf8e5eb1952d21af1ada5fc84
Ferry Roland <101640525+ferryszhtan81@users.noreply.github.com>
2022-03-22 22:49:42 +0800

    Create Prak102.php

---
+++ modul1/Prak102.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>PRAK102.php</title>
+</head>
+<body>
+<?php
+$phi = 3.14;
+$jari2 = 4.2;
+$tinggi = 5.4;
+$panjang = 8.9;
+$lebar = 14.7;
+$sisi = 7.9;
+
+echo "<br />Tinggi = ".$tinggi;
+echo "<br />Lebar = ".$lebar;
+echo "<br />Sisi = ".$sisi;
+echo "<br /><br />Output (Volume Prisma Alas Segitiga) = ";
+echo  ((1/2*$sisi*$tinggi)*$lebar)."m<sup>3</sup>";
+?>
+</body>
+</html>
