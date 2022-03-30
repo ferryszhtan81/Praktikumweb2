35ca9f6154e1c03016efa905619418790ad879ec
Ferry Roland <101640525+ferryszhtan81@users.noreply.github.com>
2022-03-30 23:24:16 +0800

    Update Prak102.php

--- modul1/Prak102.php
+++ modul1/Prak102.php
@@ -5,18 +5,13 @@
 </head>
 <body>
 <?php
-$phi = 3.14;
-$jari2 = 4.2;
 $tinggi = 5.4;
-$panjang = 8.9;
 $lebar = 14.7;
 $sisi = 7.9;
-
 echo "Tinggi = ".$tinggi;
 echo "<br />Lebar = ".$lebar;
 echo "<br />Sisi = ".$sisi;
-echo "<br /><br />Output (Volume Prisma Alas Segitiga) = ";
-echo  ((1/2*$sisi*$tinggi)*$lebar)."m<sup>3</sup>";
+echo "<br /><br />Output (Volume Prisma Alas Segitiga) = ".((1/2*$sisi*$tinggi)*$lebar)." m<sup>3</sup>";
 ?>
 </body>
 </html>
